e58d87351108970e3f78e5478c9bf10e3d31a88b
syki66 <66syki@gmail.com>
2018-02-09 00:59:58 +0900

    add cookierun_crystal_calculator.c

---
+++ cookierun_crystal_calculator.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+
+//쿠키런 크리스탈 수확량 예측기
+
+int main(void)
+{
+	int days = 0;
+	double sum_oneday_crystal = 0;
+	double oneday_crystal = 0;
+	double all_treasure_amount = 110; /*최고급보물상자에서 뽑아지는 보물만 해당됨*/
+	double all_crystal_treasure_amount = 9; /*최고급보물상자에서 뽑아지는 크리스탈 보물만 해당*/
+	double crystal_treasure_expectation_avg = (9.12 / 9); /*최고급보물상자 크리스탈보물 기댓값*/
+	double crystal_treasure_get_probability = 9 / 110 * 7; /*6+1개 세트로 파는 최고급보물상자 기준*/
+	double one_click_crystal_get = crystal_treasure_get_probability * crystal_treasure_expectation_avg; /*6+1클릭시 크리스탈 얻는 기댓값*/
+	double rest_oneday_crystal = 0;
+
+	printf("주의사항:119이상을 입력하면 값이 정확하지 않음\n");
+	printf("하루평균 버는 크리스탈량 기댓값(출석체크 1크리스탈 포함해서 입력 ㄱㄱ):");
+	scanf_s("%lf", &oneday_crystal);
+
+	while (sum_oneday_crystal<119)
+	{
+		sum_oneday_crystal = sum_oneday_crystal + oneday_crystal;
+		if (sum_oneday_crystal >= 119)
+		{
+			sum_oneday_crystal = sum_oneday_crystal - 119;
+			oneday_crystal = oneday_crystal + 0.580363;
+		}
+		days++;
+	}
+
+	printf("%d일 후에 하루에 한번씩 6+1뽑을수 있음 수고봉~", days);
+
+		return 0;
+}
