e69d2d2619abd42dd777ec9ccd32b0d45e5272a9
syki66 <66syki@gmail.com>
2025-01-04 19:01:25 +0900

    feat: 가챠 및 파일 로드 함수 분리, json 데이터 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import { itemProps } from '@/types/item';
-import { getFileData } from '@/lib/api';
-import { pickItemFromWeightedRandom } from '@/lib/random';
+import { getPickedItem } from '@/lib/gacha';
 
 export default async function Home() {
   let crystalsPerDay = 100; // 하루당 크리스탈 기댓값
   let crystals = 0; // 현재 보유한 크리스탈 개수
-
-  const multiLootboxData = await getFileData('/data/multiLootboxData.json'); // 6+1개 세트 구매했을때의 S보물 및 A보물 등장 확률 데이터
 
   for (let i = 0; i < 100; i++) {
     crystals += crystalsPerDay; // 하루당 크리스탈 획득
@@ -15,34 +11,41 @@ export default async function Home() {
     if (crystals >= 335) {
       crystals -= 335;
 
-      // 6+1세트 가중치 총합
-      const totalWeight = multiLootboxData.reduce(
-        (sum: number, item: itemProps) => sum + item.weight,
-        0
+      const pickedMultiLootbox = await getPickedItem(
+        '/data/multiLootboxData.json' // 최고급 보물 상자 6+1개 세트 뽑기 데이터
       );
 
       // 6+1세트 구매시 S보물 및 A보물 등장 확률별 분기 처리
-      switch (pickItemFromWeightedRandom(multiLootboxData, totalWeight)) {
-        case multiLootboxData[0].name:
-          console.log('S등급 보물 3개 + A등급 보물 4개');
+      switch (pickedMultiLootbox) {
+        case 'S등급 보물 3개 + A등급 보물 4개':
+          const [S1, S2, S3, A1, A2, A3, A4] = await Promise.all([
+            getPickedItem('/data/lootboxDataSGrade.json'),
+            getPickedItem('/data/lootboxDataSGrade.json'),
+            getPickedItem('/data/lootboxDataSGrade.json'),
+            getPickedItem('/data/lootboxDataAGrade.json'),
+            getPickedItem('/data/lootboxDataAGrade.json'),
+            getPickedItem('/data/lootboxDataAGrade.json'),
+            getPickedItem('/data/lootboxDataAGrade.json'),
+          ]);
+          console.log(S1, S2, S3, A1, A2, A3, A4);
           break;
-        case multiLootboxData[1].name:
+        case 'S등급 보물 4개 + A등급 보물 3개':
           console.log('S등급 보물 4개 + A등급 보물 3개');
           break;
-        case multiLootboxData[2].name:
+        case 'S등급 보물 5개 + A등급 보물 2개':
           console.log('S등급 보물 5개 + A등급 보물 2개');
           break;
-        case multiLootboxData[3].name:
+        case 'S등급 보물 6개 + A등급 보물 1개':
           console.log('S등급 보물 6개 + A등급 보물 1개');
           break;
-        case multiLootboxData[4].name:
+        case 'S등급 보물 7개':
           console.log('S등급 보물 7개');
           break;
         default:
-          console.log('예외 발생');
+          console.error('알 수 없는 결과:', pickedMultiLootbox); // 예외 발생 시 에러 로그 출력
       }
     }
   }
 
-  // return <>{crystals}</>;
+  return <>{crystalsPerDay}</>;
 }
---
+++ lib/gacha.ts
@@ -0,0 +1,31 @@
+import { itemProps } from '@/types/item';
+import { getFileData } from '@/lib/api';
+
+// 가중치에 따라 랜덤으로 항목을 선택해서 이름을 반환하는 함수
+const pickItemFromWeightedRandom = (
+  items: itemProps[],
+  totalWeight: number
+) => {
+  let random = Math.random() * totalWeight; // 0부터 totalWeight 사이의 랜덤 값 생성
+
+  for (let item of items) {
+    random -= item.weight; // 랜덤 값에서 가중치를 차감
+    if (random <= 0) {
+      return item.name; // 가중치가 초과된 항목 반환
+    }
+  }
+};
+
+// 가중치 총합을 반환하는 함수
+const getWeightedTotal = (items: itemProps[]) =>
+  items.reduce((sum: number, item: itemProps) => sum + item.weight, 0);
+
+// 가챠 뽑기 함수
+export const getPickedItem = async (path: string) => {
+  const lootboxData = await getFileData(path);
+  const pickedItem = pickItemFromWeightedRandom(
+    lootboxData,
+    getWeightedTotal(lootboxData)
+  );
+  return pickedItem;
+};
