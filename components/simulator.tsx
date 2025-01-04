e98be73ec0959c43aef041f2f496d3c3448d1086
syki66 <66syki@gmail.com>
2025-01-04 22:56:14 +0900

    feat: 시뮬레이션 로직 추가

--- components/simulator.tsx
+++ components/simulator.tsx
@@ -14,76 +14,123 @@ const Simulator: React.FC = () => {
   const [crystalsPerDay, setCrystalsPerDay] = useState(100); // 하루당 크리스탈 획득 기댓값
   const [date, setDate] = useState(0); // 현재 날짜
   const [inventory, setInventory] = useState<inventoryProps[]>([
-    { name: '기타 크리스탈 보유효과', count: 10 },
+    { name: '기타 크리스탈 보유효과 보물', count: 1, expectedValue: 10 },
   ]); // 보물 별 수량 데이터
 
   useEffect(() => {
-    // 크리스탈이 335개 이상이면 최고급 보물 상자 6+1개 세트 구매
-    let _crystals = 0;
-    let _crystalsPerDay = 100;
-    let _date = 0;
     let _inventory: inventoryProps[] = [
-      { name: '기타 크리스탈 보유효과', count: 10 },
+      { name: '기타 크리스탈 보유효과', count: 1, expectedValue: 10 },
     ];
+    let _crystals = 0;
+    let _crystalsPerDay = 0;
+    let _date = 0;
 
     const intervalId = setInterval(() => {
       _date++;
+      _crystalsPerDay = _inventory.reduce((sum, item) => {
+        if (item.name.includes('크리스탈') && item.expectedValue) {
+          return sum + item.count * item.expectedValue;
+        }
+        return sum; // "크리스탈"이 포함되지 않은 경우 기존 sum 반환
+      }, 200); // 초기값;
+
       _crystals += _crystalsPerDay;
       if (_crystals >= 335) {
-        _crystals -= 335;
+        const loopCount = Math.floor(_crystals / 335); // 몫
+        _crystals = _crystals % 335; // 나머지
 
-        const pickedMultiLootbox = getPickedItem(
-          multiLootboxData // 최고급 보물 상자 6+1개 세트 뽑기 데이터
-        );
-
-        // 6+1세트 구매시 S보물 및 A보물 등장 확률별 분기 처리
-        const pickedItems = [];
-        switch (pickedMultiLootbox) {
-          case 'S등급 보물 3개 + A등급 보물 4개':
-            pickedItems.push(
-              getPickedItem(lootboxDataSGrade),
-              getPickedItem(lootboxDataSGrade),
-              getPickedItem(lootboxDataSGrade),
-              getPickedItem(lootboxDataAGrade),
-              getPickedItem(lootboxDataAGrade),
-              getPickedItem(lootboxDataAGrade),
-              getPickedItem(lootboxDataAGrade)
-            );
-            break;
-          case 'S등급 보물 4개 + A등급 보물 3개':
-            console.log('S등급 보물 4개 + A등급 보물 3개');
-            break;
-          case 'S등급 보물 5개 + A등급 보물 2개':
-            console.log('S등급 보물 5개 + A등급 보물 2개');
-            break;
-          case 'S등급 보물 6개 + A등급 보물 1개':
-            console.log('S등급 보물 6개 + A등급 보물 1개');
-            break;
-          case 'S등급 보물 7개':
-            console.log('S등급 보물 7개');
-            break;
-          default:
-            console.error('알 수 없는 결과:', pickedMultiLootbox); // 예외 발생 시 에러 로그 출력
-        }
-        pickedItems.forEach((pickedItem) => {
-          const foundIndex = _inventory.findIndex(
-            (inventoryItem) => inventoryItem.name === pickedItem
+        for (let i = 0; i < loopCount; i++) {
+          const pickedMultiLootbox = getPickedItem(
+            multiLootboxData // 최고급 보물 상자 6+1개 세트 뽑기 데이터
           );
-          if (foundIndex === -1 && pickedItem) {
-            _inventory.push({ name: pickedItem, count: 1 });
-          } else {
-            _inventory[foundIndex].count++;
+
+          // 6+1세트 구매시 S보물 및 A보물 등장 확률별 분기 처리
+          const _pickedItems = [];
+          switch (pickedMultiLootbox) {
+            case 'S등급 보물 3개 + A등급 보물 4개':
+              _pickedItems.push(
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade)
+              );
+              break;
+            case 'S등급 보물 4개 + A등급 보물 3개':
+              _pickedItems.push(
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade)
+              );
+              break;
+            case 'S등급 보물 5개 + A등급 보물 2개':
+              _pickedItems.push(
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataAGrade),
+                getPickedItem(lootboxDataAGrade)
+              );
+              break;
+            case 'S등급 보물 6개 + A등급 보물 1개':
+              _pickedItems.push(
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataAGrade)
+              );
+              break;
+            case 'S등급 보물 7개':
+              _pickedItems.push(
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade),
+                getPickedItem(lootboxDataSGrade)
+              );
+              break;
+            default:
+              console.error('알 수 없는 결과:', pickedMultiLootbox); // 예외 발생 시 에러 로그 출력
           }
-        });
 
-        console.log(_inventory);
+          _pickedItems.forEach((pickedItem) => {
+            const foundIndex = _inventory.findIndex(
+              (inventoryItem) => inventoryItem.name === pickedItem
+            );
+            if (foundIndex === -1 && pickedItem) {
+              _inventory.push({
+                name: pickedItem,
+                count: 1,
+                expectedValue: [
+                  ...lootboxDataSGrade,
+                  ...lootboxDataAGrade,
+                ].find((x) => x.name === pickedItem)?.expectedValue,
+              });
+            } else {
+              _inventory[foundIndex].count++;
+            }
+          });
+        }
       }
-      console.log(_date, _crystals);
 
-      setInventory(_inventory);
-      setCrystals(_crystals);
       setDate(_date);
-    }, 500);
+      setCrystals(_crystals);
+      setCrystalsPerDay(_crystalsPerDay);
+      setInventory(_inventory);
+    }, 200);
 
     return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 clearInterval
   }, []);
@@ -91,7 +138,9 @@ const Simulator: React.FC = () => {
   return (
     <>
       <h1>Simulator</h1>
-      <div>{crystals}</div>
+      <div>일수 : {date}</div>
+      <div>크리스탈 보유효과 : {crystalsPerDay}</div>
+      <div>현재 크리스탈 개수 : {crystals}</div>
     </>
   );
 };
--- types/inventory.ts
+++ types/inventory.ts
@@ -1,4 +1,5 @@
 export type inventoryProps = {
   name: string;
   count: number;
+  expectedValue?: number;
 };
