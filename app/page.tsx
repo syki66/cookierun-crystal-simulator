74082dde96a067ed32f8f4a91bf1f1c7cce5b77f
syki66 <66syki@gmail.com>
2025-01-04 16:57:34 +0900

    feat: 시뮬레이션 기본 틀 추가 및 세트 가챠 확률 분기 처리

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,48 @@
-import Image from "next/image";
+import { itemProps } from '@/types/item';
+import { getFileData } from '@/lib/api';
+import { pickItemFromWeightedRandom } from '@/lib/random';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default async function Home() {
+  let crystalsPerDay = 100; // 하루당 크리스탈 기댓값
+  let crystals = 0; // 현재 보유한 크리스탈 개수
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  const multiLootboxData = await getFileData('/app/data/multiLootboxData.json'); // 6+1개 세트 구매했을때의 S보물 및 A보물 등장 확률 데이터
+
+  for (let i = 0; i < 100; i++) {
+    crystals += crystalsPerDay; // 하루당 크리스탈 획득
+
+    // 크리스탈이 335개 이상이면 최고급 보물 상자 6+1개 세트 구매
+    if (crystals >= 335) {
+      crystals -= 335;
+
+      // 6+1세트 가중치 총합
+      const totalWeight = multiLootboxData.reduce(
+        (sum: number, item: itemProps) => sum + item.weight,
+        0
+      );
+
+      // 6+1세트 구매시 S보물 및 A보물 등장 확률별 분기 처리
+      switch (pickItemFromWeightedRandom(multiLootboxData, totalWeight)) {
+        case multiLootboxData[0].name:
+          console.log('S등급 보물 3개 + A등급 보물 4개');
+          break;
+        case multiLootboxData[1].name:
+          console.log('S등급 보물 4개 + A등급 보물 3개');
+          break;
+        case multiLootboxData[2].name:
+          console.log('S등급 보물 5개 + A등급 보물 2개');
+          break;
+        case multiLootboxData[3].name:
+          console.log('S등급 보물 6개 + A등급 보물 1개');
+          break;
+        case multiLootboxData[4].name:
+          console.log('S등급 보물 7개');
+          break;
+        default:
+          console.log('예외 발생');
+      }
+    }
+  }
+
+  // return <>{crystals}</>;
 }
