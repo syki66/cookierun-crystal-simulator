1cbb2bb6f612a572f834ad7b5070b37b5ff6917f
syki66 <66syki@gmail.com>
2025-01-08 03:10:11 +0900

    feat: 시뮬레이터 페이지에서 url 파라미터 없을 경우 기본값으로 실행

--- app/simulator/show/page.tsx
+++ app/simulator/show/page.tsx
@@ -8,14 +8,14 @@ interface PageProps {
 
 export default function Page({ searchParams }: PageProps) {
   const {
-    timestamp,
-    skip,
-    speed,
-    threshold,
-    crystals,
-    defaultCrystal,
-    currentCrystals,
-  } = searchParams;
+    timestamp = Date.now(),
+    skip = 1,
+    speed = 1,
+    threshold = 1,
+    crystals = '0,0,0,0,0,0,0,0,0',
+    defaultCrystal = 1,
+    currentCrystals = 0,
+  } = searchParams; // url 파라미터가 존재하면 그대로 쓰고, 없다면 기본값 대입
 
   const numberedCrystals = String(crystals).split(',').map(Number); // 숫자 배열로 변환
   const initInventory = initInventoryData(
