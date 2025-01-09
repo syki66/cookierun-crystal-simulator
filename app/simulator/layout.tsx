059d9da8393ab116d18484644bc3c3c90c502de5
syki66 <66syki@gmail.com>
2025-01-10 03:19:59 +0900

    카카오 애드핏 컴포넌트 수정 및 광고단위 추가

--- app/simulator/layout.tsx
+++ app/simulator/layout.tsx
@@ -12,10 +12,16 @@ const KakaoAdfit = dynamic(() => import('@/components/kakao-adfit'), {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
-      <h1 className="text-4xl md:text-7xl break-keep font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-center mt-5 mb-10">
+      {/* pc용 */}
+      <KakaoAdfit adUnit={'DAN-u57341CMIeyBXond'} adSize={['728', '90']} />
+      {/* 모바일 pc 겸용 */}
+      <KakaoAdfit adUnit={'DAN-F0gt5sOnJ8XI8YL6'} adSize={['300', '250']} />
+      {/* 모바일용 */}
+      <KakaoAdfit adUnit={'DAN-xmOa3sTTtsSKKgUD'} adSize={['320', '100']} />
+
+      <h1 className="text-4xl md:text-7xl break-keep font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-center mt-10 mb-10">
         쿠키런 크리스탈 기댓값 시뮬레이터
       </h1>
-      <KakaoAdfit adUnit={'DAN-u57341CMIeyBXond'} />
       {children}
     </>
   );
--- components/kakao-adfit.tsx
+++ components/kakao-adfit.tsx
@@ -4,9 +4,10 @@ import React, { useEffect } from 'react';
 
 type KakaoAdfitProps = {
   adUnit: string;
+  adSize: ['300', '250'] | ['728', '90'] | ['320', '100'];
 };
 
-export default function KakaoAdfit({ adUnit }: KakaoAdfitProps) {
+export default function KakaoAdfit({ adUnit, adSize }: KakaoAdfitProps) {
   useEffect(() => {
     const script = document.createElement('script');
     script.async = true;
@@ -15,12 +16,14 @@ export default function KakaoAdfit({ adUnit }: KakaoAdfitProps) {
   }, [adUnit]);
 
   return (
-    <ins
-      className="kakao_ad_area"
-      style={{ display: 'none' }}
-      data-ad-unit={adUnit}
-      data-ad-width="728"
-      data-ad-height="90"
-    ></ins>
+    <div className="text-center">
+      <ins
+        className="kakao_ad_area flex"
+        style={{ display: 'none' }}
+        data-ad-unit={adUnit}
+        data-ad-width={adSize[0]}
+        data-ad-height={adSize[1]}
+      ></ins>
+    </div>
   );
 }
