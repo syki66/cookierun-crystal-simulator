6d49acd2326e20b295ce7963fac6161397b909e0
syki66 <66syki@gmail.com>
2025-01-19 03:01:20 +0900

    feat: 시뮬레이션 페이지 주석 컴포넌트 공통화

--- app/simulator/layout.tsx
+++ app/simulator/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import dynamic from 'next/dynamic';
+import Note from '@/components/note';
 
 export const metadata: Metadata = {
   title: '쿠키런 크리스탈 시뮬레이션',
@@ -31,6 +32,27 @@ export default function Layout({ children }: LayoutProps) {
       </div>
 
       {children}
+
+      <div className="max-w-screen-md mx-auto mt-10">
+        <Note>
+          * 쿠키런 크리스탈 기댓값 시뮬레이션은{' '}
+          <a
+            href="https://cookierun.zendesk.com/hc/ko/articles/28813434627993-%EC%83%81%EC%84%B8%EC%A0%95%EB%B3%B4"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            쿠키런 고객센터에 공개된 확률정보
+          </a>
+          를 바탕으로 제작되었습니다.
+          <br />* 정상적인 범위를 벗어난 숫자를 입력하거나 오랜시간 시뮬레이션을
+          실행하면 웹 브라우저가 느려지거나 정지할 수 있습니다.
+          <br />* 보물상자 오픈 트리거가 발동하게 되면 현재 보유하고 있는
+          크리스탈이 최대로 소진됩니다. <br />* 크리스탈 기댓값을 계산할 때,
+          보물은 +9강으로 가정하고 계산됩니다.
+          <br />* 하루당 크리스탈 획득량은 반올림되어 자연수로 표기됩니다.
+        </Note>
+      </div>
 
       <div className="mt-5">
         <KakaoAdfit adUnit={'DAN-F0gt5sOnJ8XI8YL6'} adSize={['300', '250']} />
--- app/simulator/ready/page.tsx
+++ app/simulator/ready/page.tsx
@@ -36,7 +36,6 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { crystalItemsData } from '@/data/lootboxData';
-import Note from '@/components/note';
 import { getExpectedValueByName } from '@/lib/gacha';
 
 const FormSchema = z.object({
@@ -468,7 +467,7 @@ export default function Page() {
 
           <Button
             type="submit"
-            className="w-full h-16 my-10 select-none text-white text-4xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-500 hover:via-pink-600 hover:to-red-600"
+            className="w-full h-16 mt-10 select-none text-white text-4xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 hover:from-purple-500 hover:via-pink-600 hover:to-red-600"
             disabled={isPending}
           >
             {isPending ? (
@@ -489,24 +488,6 @@ export default function Page() {
           </Button>
         </form>
       </Form>
-      <Note>
-        * 쿠키런 크리스탈 기댓값 시뮬레이션은{' '}
-        <a
-          href="https://cookierun.zendesk.com/hc/ko/articles/28813434627993-%EC%83%81%EC%84%B8%EC%A0%95%EB%B3%B4"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          쿠키런 고객센터에 공개된 확률정보
-        </a>
-        를 바탕으로 제작되었습니다.
-        <br />* 정상적인 범위를 벗어난 숫자를 입력하거나 오랜시간 시뮬레이션을
-        실행하면 웹 브라우저가 느려지거나 정지할 수 있습니다.
-        <br />* 보물상자 오픈 트리거가 발동하게 되면 현재 보유하고 있는
-        크리스탈이 최대로 소진됩니다. <br />* 크리스탈 기댓값을 계산할 때,
-        보물은 +9강으로 가정하고 계산됩니다.
-        <br />* 하루당 크리스탈 획득량은 반올림되어 자연수로 표기됩니다.
-      </Note>
     </div>
   );
 }
